662cc3edc125cb3fb93ce8b1ccf7890d38839ea5
Steve Gillet <stephen.gillet@gmail.com>
2022-02-19 17:22:35 -0600

    added transactions rollbacks

--- models/index.js
+++ models/index.js
@@ -1,26 +1,31 @@
-'use strict';
+"use strict";
 
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
+const fs = require("fs");
+const path = require("path");
+const Sequelize = require("sequelize");
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.js')[env];
+const env = process.env.NODE_ENV || "development";
+const config = require(__dirname + "/../config/config.js")[env];
 const db = {};
 
 const sequelize = new Sequelize(config);
+const t = sequelize.transaction();
 
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+fs.readdirSync(__dirname)
+  .filter((file) => {
+    return (
+      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+    );
   })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+  .forEach((file) => {
+    const model = require(path.join(__dirname, file))(
+      sequelize,
+      Sequelize.DataTypes
+    );
     db[model.name] = model;
   });
 
-Object.keys(db).forEach(modelName => {
+Object.keys(db).forEach((modelName) => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
--- routes/users.js
+++ routes/users.js
@@ -1,82 +1,97 @@
-const express = require('express');
-const {User, Ieee} = require('../models/index');
-const bcrypt = require('bcryptjs');
-const passport = require('passport');
-
+const express = require("express");
+const { User, Ieee, t } = require("../models/index");
+const bcrypt = require("bcryptjs");
+const passport = require("passport");
 
 // Handle any routes related to users -> /users
 const router = express.Router();
 
 // Get all users.
-router.get('/all', passport.authenticate('jwt', {session : false}),async(req, res) => {
+router.get(
+  "/all",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
     try {
-        // If its an officer, print out all users, else print own user.
-        if(req.user.role){
-            let users = await User.findAll()
-                .catch( (err) => {
-                    console.error(err);
-                });
-            res.status(200).json(users);
-        }else{
-            res.status(401).json({msg : 'Unauthorized. You need to be an admin.'});
-        }
-    }catch(err){
-        next(err);
+      // If its an officer, print out all users, else print own user.
+      if (req.user.role) {
+        let users = await User.findAll().catch((err) => {
+          console.error(err);
+        });
+        res.status(200).json(users);
+      } else {
+        res.status(401).json({ msg: "Unauthorized. You need to be an admin." });
+      }
+    } catch (err) {
+      next(err);
     }
-});
+  }
+);
 
 // Find specific user by id
-router.get('/:id', passport.authenticate('jwt', {session : false}), async(req, res, next) => {
+router.get(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res, next) => {
     try {
-        if(req.user.role){
-            const user = await User.findByPk(req.params.id);
-            res.status(200).json(user);
-        }else {
-            res.status(401).send('Unauthorized');
-        }
-    }catch(err){
-        next(err);
+      if (req.user.role) {
+        const user = await User.findByPk(req.params.id);
+        res.status(200).json(user);
+      } else {
+        res.status(401).send("Unauthorized");
+      }
+    } catch (err) {
+      next(err);
     }
-});
+  }
+);
 
 // Update user profile
-router.put('/:id', passport.authenticate('jwt', {session : false}),async(req, res, next) => {
-    try{
-        if (req.user.id == req.params.id) {
-          return res.status(200).json(member);
-        } else if (req.user.role) {
-          Ieee.findOne({ where: { UserId: req.params.id } })
-            .then(function (ieee) {
-              if (ieee) {
-                res.status(200).json(member);
-              } else {
-                res.status(404).json({ msg: "IEEE member not found" });
-              }
-            })
-            .catch(next);
-        }else{
-            res.status(401).send('Unauthorized');
-        }
-    }catch(err){
-        next(err);
+router.put(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res, next) => {
+    try {
+      if (req.user.id == req.params.id) {
+        return res.status(200).json(member);
+      } else if (req.user.role) {
+        Ieee.findOne({ where: { UserId: req.params.id } })
+          .then(function (ieee) {
+            if (ieee) {
+              res.status(200).json(member);
+            } else {
+              res.status(404).json({ msg: "IEEE member not found" });
+            }
+          })
+          .catch(next);
+      } else {
+        res.status(401).send("Unauthorized");
+      }
+    } catch (err) {
+      next(err);
     }
-
-});
+  }
+);
 
 // delete a user by Id
-router.delete('/:id', passport.authenticate('jwt', {session : false}), async (req, res) => {
+router.delete(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
     try {
-        if (req.user.id == req.params.id) {
-          return res.status(401).json('Unauthorized');
-        } else if (req.user.role) {
-          await req.user.destroy();
-          res.status(200).end();
-        }else{
-            res.status(401).send('Unauthorized');
-        }
-    }catch(err){
-        next(err);
+      if (req.user.id == req.params.id) {
+        return res.status(401).json("Unauthorized");
+      } else if (req.user.role) {
+        await req.user.destroy({}, { transaction: t });
+        res.status(200).end();
+      } else {
+        res.status(401).send("Unauthorized");
+      }
+      await t.commit();
+    } catch (err) {
+      await t.rollback();
+      next(err);
     }
-});
+  }
+);
 
 module.exports = router;
