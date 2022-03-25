e665319dd6021a562ba3c4bc6b6376233b49ac71
Akhil Kapadia <Akhil.kapadia@ttu.edu>
2022-03-25 09:15:30 -0500

    update db

---
+++ migrations/20220325140959-create-token-passwords.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('token.passwords', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      token: {
+        type: Sequelize.STRING
+      },
+      expiration: {
+        type: Sequelize.DATE
+      },
+      userId: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('token.passwords');
+  }
+};
--- seeders/20220122060350-admin-user.js
+++ seeders/20220122060350-admin-user.js
@@ -10,7 +10,7 @@ module.exports = {
      firstname: 'Iris',
      lastname: 'Admin',
      email: 'ieee@ttu.edu',
-     password: await bcrypt.hash("zqP$sGf5", 10),
+     password: "$2a$10$qESXwfc5ObrOyhJyhqPo8OGVwpJICVU1P5b.AnH6VT4pxp0WdSUNq",
      createdAt: new Date(),
      updatedAt: new Date()
    }], {transaction: t});
