301d9ca552522e7ddae5e15be12fd38baa6ba871
mertzmel <99104698+mertzmel@users.noreply.github.com>
2022-03-28 11:23:32 -0500

    Stable Email Sender
    Needs to be reworked into routes
    JWT functionality needs to be added

--- app.js
+++ app.js
@@ -13,10 +13,6 @@ const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
 const route = express.Router();
 
-const crypto = require('crypto');
-const {User, Ieee} = require('./models/index');
-
-
 //config
 // require('./config/db');
 require('./models/index');
@@ -69,78 +65,25 @@ const transporter = nodemailer.createTransport({
         user: 'ttuieee2022@gmail.com',
         pass: 'ttuieee1923@!',
     }
-    });
+});
 
-    route.post('/forgotPassword', (req, res) => {
-        if (req.body.email === '') {
-          res.status(400).send('email required');
+route.post('/text-mail', (req, res) => {
+    const {to, subject, text } = req.body;
+    const mailData = {
+        from: 'ttuieee2022@gmail.com',
+        to: 'melanie.mertzlufft@gmail.com',
+        subject: 'Email Test',
+        text: 'That was easy!',
+        html: '<b>Hey there! </b><br> This is our first message sent with Nodemailer<br/>',
+    };
+
+    transporter.sendMail(mailData, (error, info) => {
+        if (error) {
+            return console.log(error);
         }
-        console.error(req.body.email);
-        User.findOne({
-          where: {
-            email: req.body.email,
-          },
-        }).then((user) => {
-          if (user === null) {
-            console.error('email not in database');
-            res.status(403).send('email not in db');
-          } else {
-            const token = crypto.randomBytes(20).toString('hex');
-            User.update({
-              resetPasswordToken: token,
-              resetPasswordExpires: Date.now() + 3600000,
-            });
-    
-            const transporter = nodemailer.createTransport({
-              service: 'gmail',
-              auth: {
-                user: 'ttuieee2022@gmail.com',
-                pass: 'ttuieee1923@!',
-              },
-            });
-    
-            const mailOptions = {
-              from: 'ttuieee2022@gmail.com',
-              to: 'melanie.mertzlufft@gmail.com',
-              subject: 'Link To Reset Password',
-              text:
-                'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n'
-                + 'Please click on the following link, or paste this into your browser to complete the process within one hour of receiving it:\n\n'
-                + `http://localhost:3031/reset/${token}\n\n`
-                + 'If you did not request this, please ignore this email and your password will remain unchanged.\n',
-            };
-    
-            console.log('sending mail');
-    
-            transporter.sendMail(mailOptions, (err, response) => {
-              if (err) {
-                console.error('there was an error: ', err);
-              } else {
-                console.log('here is the res: ', response);
-                res.status(200).json('recovery email sent');
-              }
-            });
-          }
-        });
-      });    
-
-// route.post('/text-mail', (req, res) => {
-//     const {to, subject, text } = req.body;
-//     const mailData = {
-//         from: 'ttuieee2022@gmail.com',
-//         to: 'melanie.mertzlufft@gmail.com',
-//         subject: 'Email Test',
-//         text: 'That was easy!',
-//         html: '<b>Hey there! </b><br> This is our first message sent with Nodemailer<br/>',
-//     };
-
-//     transporter.sendMail(mailData, (error, info) => {
-//         if (error) {
-//             return console.log(error);
-//         }
-//         res.status(200).send({ message: "Mail send", message_id: info.messageId });
-//     });
-// });
+        res.status(200).send({ message: "Mail send", message_id: info.messageId });
+    });
+});
 
 // Routes
 const userRoute = require('./routes/users');
